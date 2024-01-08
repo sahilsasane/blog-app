1439ce6c85b823e78958164f66cf3208c2e52e93
sahil <shsasane22@gmail.com>
2024-01-08 19:55:21 +0530

    navbar done

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./footer.module.css";
+import Image from "next/image";
+import Link from "next/link";
+
+const Footer = () => {
+  return (
+    <div className={styles.container}>
+      Footer
+    </div>
+  );
+};
+
+export default Footer;
